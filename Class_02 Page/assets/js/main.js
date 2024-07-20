8d7cbf9e434aaf94c230e8c4b0db4757468f23ba
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-07-20 20:51:30 +0400

    save

---
+++ Class_02 Page/assets/js/main.js
@@ -0,0 +1,10 @@
+const barsBtn = document.querySelector(".header .openCloseBtns .bars");
+const closeBtn = document.querySelector(".closeBtn");
+const getBtnAndBar = document.querySelector(".openCloseBtns .getBtnAndBar");
+
+// Tablet
+// const tabletMenu = document.querySelector(".tabletMenu");
+const header = document.querySelector("div.header");
+const tClassDropdown = document.querySelector(".tClassDropdown");
+const subMenu = document.querySelector(".tabletMenu .subMenu ");
+const outbound = document.querySelector(".outbound");
