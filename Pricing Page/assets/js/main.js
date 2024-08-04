57865511e828218bf048fce73e3ab0bb938def02
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-04 16:08:56 +0400

    save

--- Pricing Page/assets/js/main.js
+++ Pricing Page/assets/js/main.js
@@ -1,15 +1,13 @@
-let preveiwContainer = document.querySelector(".faqList");
-let faqItemBody = preveiwContainer.querySelectorAll(".faqItemBody");
+const faq_item_head_all = document.querySelectorAll(".faqItem");
 
-document.querySelectorAll(".faqList .faqItem").forEach((product) => {
-  product.onclick = () => {
-    let name = product.getAttribute("data-name");
-    faqItemBody.forEach((preview) => {
-      let target = preview.getAttribute("data-target");
-      if (name == target) {
-        preview.classList.add("active");
-      }
-    });
-  };
+faq_item_head_all.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    // rotate icon
+    item
+      .querySelector(".faqItemHead i.fa-angle-down")
+      .classList.toggle("rotate");
+
+    //  show body
+    item.querySelector(".faqItemBody").classList.toggle("active");
+  });
 });
-
