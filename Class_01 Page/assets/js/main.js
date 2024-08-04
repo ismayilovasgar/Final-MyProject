a66c5cdd7238b8eaf8f82b9ac566ecb9cf3a9c9e
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-04 14:25:45 +0400

    save filter items

--- Class_01 Page/assets/js/main.js
+++ Class_01 Page/assets/js/main.js
@@ -1,20 +1,36 @@
-const select = document.querySelector(".select");
-const spanText = document.querySelector(".select span.current");
-const arrows = document.querySelectorAll(".select .select_arrow i");
-const list = document.querySelector("ul.list");
-const listItems = document.querySelectorAll("ul.list li");
+//* nice-select
+const select = document.querySelector(".nice-select");
+const spanText = document.querySelector(".nice-select input.current");
+const arrows = document.querySelectorAll(".nice-select .select_arrow i");
+const list = document.querySelector(".nice-select ul.list");
+const listItems = document.querySelectorAll(".nice-select ul.list li");
+
 select.addEventListener("click", (e) => {
   list.classList.toggle("show");
   select.classList.toggle("focus");
 
   arrows.forEach((arrow) => {
     arrow.classList.toggle("changeDirection");
-    console.log(arrow);
   });
 
   listItems.forEach((item) => {
     item.addEventListener("click", (e) => {
-      spanText.textContent = item.innerHTML;
+      spanText.value = item.textContent;
+    });
+  });
+});
+
+//* sorting-select
+const select_sorting = document.querySelectorAll(".sorting");
+const sorting_list_items = null;
+select_sorting.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    item.querySelector("i").classList.toggle("changeDirection");
+    item.classList.toggle("focus");
+    item.querySelector("ul.list").classList.toggle("show");
+    sorting_list_items = item.querySelectorAll("ul li");
+    sorting_list_items.forEach(list_item => {
+        
     });
   });
 });
