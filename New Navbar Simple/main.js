4f175f421761e912766cc5d271d80258e44503c5
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-13 01:50:39 +0400

    save

--- New Navbar Simple/main.js
+++ New Navbar Simple/main.js
@@ -1,47 +1,43 @@
 // Desktop
-const headerHead = document.querySelector(".header .headerHead");
-const headerBody = document.querySelector(" .headerBody");
-const headerDropdownArrows = document.querySelectorAll(".headerHead i");
+const dropdown = document.querySelector("li.drop");
+const dropdownArrow = document.querySelector("li.drop > i");
 
-headerHead.addEventListener("click", (e) => {
-  headerBody.classList.toggle("active");
-  headerDropdownArrows.forEach((i) => {
-    i.classList.toggle("active");
-  });
+dropdown.addEventListener("mouseover", (e) => {
+  dropdownArrow.classList.toggle("rotate_180");
 });
 
 // Tablet
-const btnBars = document.querySelector(".headerCenter button.bars");
-const btnClose = document.querySelector(".headerCenter button.close");
-const tabletMenu = document.querySelector("section.header .tabletMenu");
+const btnBars = document.querySelector(".headerCenter button.bars i");
+const btnClose = document.querySelector(".headerCenter button.bars i.fa-xmark");
+const tabletMenu = document.querySelector(".header .tabletMenu");
 const mobileMenu = document.querySelector("section.header .mobileMenu");
 const dropdownLink = document.querySelector(".tabletMenu ul > li.dropdownLink");
 const dropdownBody = document.querySelector(".tabletMenu .dropdownBody");
+
 btnBars.addEventListener("click", (e) => {
-  btnClose.style.display = "block";
-  btnBars.style.display = "none";
+  btnBars.classList.toggle("fa-xmark");
+  tabletMenu.style.display = "block";
 
-  if (window.screen.width > 767 && window.screen.width <= 1023) {
-    tabletMenu.style.display = "block";
-  } else {
-    mobileMenu.style.display = "block";
-  }
-
-  e.preventDefault();
-});
-
-btnClose.addEventListener("click", (e) => {
-  btnClose.style.display = "none";
-  btnBars.style.display = "block";
-
-  if (window.screen.width > 767 && window.screen.width <= 1023) {
+  if (!btnBars.classList.contains("fa-xmark")) {
     tabletMenu.style.display = "none";
   } else {
-    mobileMenu.style.display = "none";
+    tabletMenu.style.display = "block";
   }
 
   e.preventDefault();
 });
+
+// btnClose.addEventListener("click", (e) => {
+//   tabletMenu.style.display = "none";
+//   console.log("+++");
+//   if (window.screen.width > 767 && window.screen.width <= 1023) {
+//     tabletMenu.style.display = "none";
+//   } else {
+//     mobileMenu.style.display = "none";
+//   }
+
+//   e.preventDefault();
+// });
 
 dropdownLink.addEventListener("click", (e) => {
   dropdownLink.querySelector("i").classList.toggle("active");
@@ -49,14 +45,14 @@ dropdownLink.addEventListener("click", (e) => {
 });
 
 //  Mobile
-const mobileHead = document.querySelector(".mobileMenu .mobileHead");
-const mobileHeadIcon = document.querySelector(".mobileMenu .mobileHead i");
-const mobileBody = document.querySelector(".mobileMenu .mobileBody");
-const mobileNav = document.querySelectorAll(".mobileMenu .mobileNav");
-mobileHead.addEventListener("click", (e) => {
-  mobileHead.querySelector(".mobileLink").classList.toggle("selected");
-  mobileHeadIcon.classList.toggle("rotate_180");
-  mobileBody.classList.toggle("active");
+// const mobileHead = document.querySelector(".mobileMenu .mobileHead");
+// const mobileHeadIcon = document.querySelector(".mobileMenu .mobileHead i");
+// const mobileBody = document.querySelector(".mobileMenu .mobileBody");
+// const mobileNav = document.querySelectorAll(".mobileMenu .mobileNav");
+// mobileHead.addEventListener("click", (e) => {
+//   mobileHead.querySelector(".mobileLink").classList.toggle("selected");
+//   mobileHeadIcon.classList.toggle("rotate_180");
+//   mobileBody.classList.toggle("active");
 
-  e.preventDefault();
-});
+//   e.preventDefault();
+// });
