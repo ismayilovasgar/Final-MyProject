b12cbbf6418c17f3fe99129b67d55a43f920d259
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-12 18:59:06 +0400

    save_08_12

--- New Navbar Simple/main.js
+++ New Navbar Simple/main.js
@@ -1,4 +1,4 @@
-//
+// Desktop
 const headerHead = document.querySelector(".header .headerHead");
 const headerBody = document.querySelector(" .headerBody");
 const headerDropdownArrows = document.querySelectorAll(".headerHead i");
@@ -10,12 +10,13 @@ headerHead.addEventListener("click", (e) => {
   });
 });
 
-//
+// Tablet
 const btnBars = document.querySelector(".headerCenter button.bars");
 const btnClose = document.querySelector(".headerCenter button.close");
 const tabletMenu = document.querySelector("section.header .tabletMenu");
 const mobileMenu = document.querySelector("section.header .mobileMenu");
-
+const dropdownLink = document.querySelector(".tabletMenu ul > li.dropdownLink");
+const dropdownBody = document.querySelector(".tabletMenu .dropdownBody");
 btnBars.addEventListener("click", (e) => {
   btnClose.style.display = "block";
   btnBars.style.display = "none";
@@ -42,7 +43,12 @@ btnClose.addEventListener("click", (e) => {
   e.preventDefault();
 });
 
-//
+dropdownLink.addEventListener("click", (e) => {
+  dropdownLink.querySelector("i").classList.toggle("rotate_180");
+  dropdownBody.classList.toggle("active");
+});
+
+//  Mobile
 const mobileHead = document.querySelector(".mobileMenu .mobileHead");
 const mobileHeadIcon = document.querySelector(".mobileMenu .mobileHead i");
 const mobileBody = document.querySelector(".mobileMenu .mobileBody");
