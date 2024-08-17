95b4abf44da43281b096028516d39078b751edec
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-17 21:52:53 +0400

    save

--- Class page/assets/js/main.js
+++ Class page/assets/js/main.js
@@ -1,10 +1,19 @@
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
-    type: "fraction",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    clickable: true,
   },
 });
---
+++ Test/swipper/index.js
@@ -0,0 +1,6 @@
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".direction.right",
+    prevEl: ".direction.left",
+  },
+});
