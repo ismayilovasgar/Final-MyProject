d3af88af751b4e0b56d665fdfc5a8de7128298bc
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-19 20:54:14 +0400

    save

--- Class_02 Page/assets/js/main.js
+++ Class_02 Page/assets/js/main.js
@@ -18,3 +18,26 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+// ! Swiper - 1
+var swiper = new Swiper(".programs_swiper", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".programs_next_btn",
+    prevEl: ".programs_prev_btn",
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
