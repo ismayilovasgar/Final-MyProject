e52062d7faf011d6a6563029690a4724f2cc027c
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-08-21 02:23:41 +0400

    save

--- New Navbar Simple/main.js
+++ New Navbar Simple/main.js
@@ -34,3 +34,27 @@ function clickOutside(e) {
 }
 document.addEventListener("click", clickOutside);
 
+// change theme icon
+const toggle_button = document.getElementById("dark-change");
+toggle_button.addEventListener("click", function () {
+  document.body.classList.add("dark_theme");
+  toggle_button.classList.toggle("active");
+  // if (document.body.classList.contains("dark-theme")) {
+  //   hidden_all_item(light_images);
+  //   show_all_item(dark_images);
+  // }
+});
+
+// change footer nav
+const footer_nav = document.querySelector(
+  ".footer__col:nth-child(1) .footer__category"
+);
+const footer_nav__icon = document.querySelector(
+  ".footer__col:nth-child(1) .footer__category i"
+);
+const footer_menu = document.querySelector(".footer__menu ul");
+
+footer_nav.addEventListener("click", (e) => {
+  footer_menu.classList.toggle("active");
+  footer_nav__icon.classList.toggle("rotate");
+});
